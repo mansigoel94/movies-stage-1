87c3d558d1f572ca282df1e9c5d74e4eef0018e1
Your Name <you@example.com>
2017-08-31 21:59:02 +0530

    Seperated DetailACtivity and DetailFragment

--- app/src/main/java/com/example/mansi/movies/DetailActivity.java
+++ app/src/main/java/com/example/mansi/movies/DetailActivity.java
@@ -1,56 +1,21 @@
 package com.example.mansi.movies;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
-    TextView mSynopsis;
-    ImageView mImageView;
-    TextView mRatings;
-    TextView mReleaseDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        Movie movieToDisplay = (Movie) getIntent().getParcelableExtra(getString(R.string.open_detail_intent_key));
-
-        String title = movieToDisplay.getTitle();
-        if (title != null) {
-            setTitle(title);
-        }
-        mImageView = (ImageView) findViewById(R.id.detail_poster);
-        mSynopsis = (TextView) findViewById(R.id.detail_synopsis);
-        mRatings = (TextView) findViewById(R.id.detail_ratings);
-        mReleaseDate = (TextView) findViewById(R.id.detail_release_date);
-
-        //displaying poster
-        String relativeUrl = movieToDisplay.getPoster();
-        if (relativeUrl != null && !TextUtils.isEmpty(relativeUrl))
-            Picasso.with(getBaseContext())
-                    .load(Utility.getAbsoluteUrlForPoster(relativeUrl))
-                    .into(mImageView);
-
-        String releaseDate = movieToDisplay.getReleaseDate();
-        if (releaseDate != null && !TextUtils.isEmpty(releaseDate)) {
-            mReleaseDate.setText(Utility.formatDate(getBaseContext(), releaseDate));
-        }
-        //displaying synopsis
-        String synopsis = movieToDisplay.getSynopsis();
-        if (synopsis != null && !TextUtils.isEmpty(synopsis))
-            mSynopsis.setText(synopsis);
-
-        //displaying ratings
-        if (movieToDisplay.getRatings() != -1) {
-            String ratings = String.format(getString(R.string.format_ratings), movieToDisplay.getRatings());
-            mRatings.setText(ratings);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new DetailActivityFragment())
+                    .commit();
         }
     }
 }
--- app/src/main/java/com/example/mansi/movies/DetailActivityFragment.java
+++ app/src/main/java/com/example/mansi/movies/DetailActivityFragment.java
@@ -1,15 +1,25 @@
 package com.example.mansi.movies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
+
+    TextView mSynopsis;
+    ImageView mImageView;
+    TextView mRatings;
+    TextView mReleaseDate;
 
     public DetailActivityFragment() {
     }
@@ -17,6 +27,42 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Movie movieToDisplay = (Movie) getActivity().getIntent()
+                .getParcelableExtra(getString(R.string.open_detail_intent_key));
+
+        String title = movieToDisplay.getTitle();
+        if (title != null) {
+            getActivity().setTitle(title);
+        }
+        mImageView = (ImageView) rootView.findViewById(R.id.detail_poster);
+        mSynopsis = (TextView) rootView.findViewById(R.id.detail_synopsis);
+        mRatings = (TextView) rootView.findViewById(R.id.detail_ratings);
+        mReleaseDate = (TextView) rootView.findViewById(R.id.detail_release_date);
+
+        //displaying poster
+        String relativeUrl = movieToDisplay.getPoster();
+        if (relativeUrl != null && !TextUtils.isEmpty(relativeUrl))
+            Picasso.with(getContext())
+                    .load(Utility.getAbsoluteUrlForPoster(relativeUrl))
+                    .into(mImageView);
+
+        String releaseDate = movieToDisplay.getReleaseDate();
+        if (releaseDate != null && !TextUtils.isEmpty(releaseDate)) {
+            mReleaseDate.setText(Utility.formatDate(getContext(), releaseDate));
+        }
+        //displaying synopsis
+        String synopsis = movieToDisplay.getSynopsis();
+        if (synopsis != null && !TextUtils.isEmpty(synopsis))
+            mSynopsis.setText(synopsis);
+
+        //displaying ratings
+        if (movieToDisplay.getRatings() != -1) {
+            String ratings = String.format(getString(R.string.format_ratings), movieToDisplay.getRatings());
+            mRatings.setText(ratings);
+        }
+        return rootView;
     }
 }

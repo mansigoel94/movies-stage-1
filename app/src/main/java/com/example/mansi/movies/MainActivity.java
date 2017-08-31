fcac99946a738344cf3ae2b1bf7a56a6420a181c
Your Name <you@example.com>
2017-08-31 17:45:29 +0530

    Added blank settings activity and Menu option in Main activity

--- app/src/main/java/com/example/mansi/movies/MainActivity.java
+++ app/src/main/java/com/example/mansi/movies/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mansi.movies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new MainDisplayFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = new MenuInflater(getBaseContext());
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings_menu_item:
+                Intent openSettingsActivity = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(openSettingsActivity);
+                break;
+        }
+        return true;
     }
 }

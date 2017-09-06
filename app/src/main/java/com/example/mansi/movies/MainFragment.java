2142c67f06c69fd0cc8053d5b5e3e7fd3c0b014b
Your Name <you@example.com>
2017-09-06 19:28:16 +0530

    Added automatic scroller to scroll to previous first item position on rotation of phone

--- app/src/main/java/com/example/mansi/movies/MainFragment.java
+++ app/src/main/java/com/example/mansi/movies/MainFragment.java
@@ -4,10 +4,11 @@ package com.example.mansi.movies;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +23,15 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
 
     private static final int LOADER_ID = 1;
     private static final String LOG_TAG = MainFragment.class.getSimpleName();
+    private static final String SCROLL_POSITION_KEY = "scroll";
+    private static final String OFFSET_KEY = "offset";
     Context context;
     boolean isResumedCalledFirstTime = true;
     private MovieAdapter mAdapter;
     private ProgressBar mProgressBar;
     private TextView mEmptyView;
     private GridView mGridview;
+    private int mPosition = 0;
 
     public MainFragment() {
         // Required empty public constructor
@@ -41,6 +45,10 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mPosition = savedInstanceState.getInt(SCROLL_POSITION_KEY);
+        }
+
         ArrayList<Movie> movieArrayList = new ArrayList<>();
 
         context = getContext();
@@ -55,6 +63,16 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
         mAdapter = new MovieAdapter(getActivity(), movieArrayList);
         mGridview.setAdapter(mAdapter);
 
+        //since smoothScrollToPosition was not working directly therefore I used delay thread
+        //I understand its not the optimal solution so please can you tell me the optimal way to do the scrolling
+        //of gridview without delaying the operation
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mGridview.smoothScrollToPosition(mPosition);
+            }
+        }, 200);
+
         mGridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -64,8 +82,21 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
                 startActivity(openDetailActivity);
             }
         });
-
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        //get first visible position
+        int index = mGridview.getFirstVisiblePosition();
+        int offset = mGridview.getTop();
+        outState.putInt(SCROLL_POSITION_KEY, index);
     }
 
     @Override
